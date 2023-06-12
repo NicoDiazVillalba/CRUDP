8f2fc1a9866c39d95c8f0dd794f480c811ff79ce
NicoDiazVillalba <90287672+NicoDiazVillalba@users.noreply.github.com>
2023-06-12 15:45:31 -0500

    Cambio en el manejo de evento spor botones

--- src/controlador/controlador.java
+++ src/controlador/controlador.java
@@ -7,7 +7,7 @@ import modelo.ConsultasPelicula;
 import modelo.Pelicula;
 import vista.vistaPrincipal;
 
-public class controlador implements ActionListener {
+public class controlador {
 
     private Pelicula peli;
     private vistaPrincipal vista;
@@ -18,84 +18,115 @@ public class controlador implements ActionListener {
         this.vista = vista;
         this.peliConsulta = peliConsulta;
         this.vista.setVisible(true);
-        this.vista.botonAgregar.addActionListener(this);
-        this.vista.botonModificar.addActionListener(this);
-        this.vista.botonEliminar.addActionListener(this);
-        this.vista.botonBuscar.addActionListener(this);
-        this.vista.botonLimpiar.addActionListener(this);
+
+        accionarBotones();
+    }
+
+    private void accionarBotones() {
+        this.vista.botonAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                agregar(evt);
+            }
+
+        });
+
+        this.vista.botonModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                modificar(evt);
+            }
+
+        });
+
+        this.vista.botonEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                eliminar(evt);
+            }
+
+        });
+
+        this.vista.botonBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buscar(evt);
+            }
+
+        });
+
+        this.vista.botonLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                limpiar(evt);
+            }
+
+        });
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private void agregar(java.awt.event.ActionEvent evt) {
+        peli.setNombre(vista.entradaNombre.getText());
+        peli.setGenero(vista.entradaGenero.getText());
+        peli.setAño(Integer.parseInt(vista.entradaAño.getText()));
+        peli.setDirector(vista.entradaDirector.getText());
+        peli.setPais(vista.entradaPais.getText());
 
-        if (e.getSource() == vista.botonAgregar) {
-            peli.setNombre(vista.entradaNombre.getText());
-            peli.setGenero(vista.entradaGenero.getText());
-            peli.setAño(Integer.parseInt(vista.entradaAño.getText()));
-            peli.setDirector(vista.entradaDirector.getText());
-            peli.setPais(vista.entradaPais.getText());
-
-            if (peliConsulta.registrar(peli)) {
-                JOptionPane.showMessageDialog(vista, "Producto Agregado");
-                limpiar();
-            } else {
-                JOptionPane.showMessageDialog(vista, "Error al agregar");
-                limpiar();
-            }
-        }
-
-        if (e.getSource() == vista.botonModificar) {
-            peli.setId(Integer.parseInt(vista.entradaID.getText()));
-            peli.setNombre(vista.entradaNombre.getText());
-            peli.setGenero(vista.entradaGenero.getText());
-            peli.setAño(Integer.parseInt(vista.entradaAño.getText()));
-            peli.setDirector(vista.entradaDirector.getText());
-            peli.setPais(vista.entradaPais.getText());
-
-            if (peliConsulta.modificar(peli)) {
-                JOptionPane.showMessageDialog(vista, "Producto modificado");
-                limpiar();
-            } else {
-                JOptionPane.showMessageDialog(vista, "Error al modificar");
-                limpiar();
-            }
-        }
-
-        if (e.getSource() == vista.botonEliminar) {
-            peli.setId(Integer.parseInt(vista.entradaID.getText()));
-
-            if (peliConsulta.eliminar(peli)) {
-                JOptionPane.showMessageDialog(vista, "Producto eliminado");
-                limpiar();
-            } else {
-                JOptionPane.showMessageDialog(vista, "Error al eliminar");
-                limpiar();
-            }
-        }
-
-        if (e.getSource() == vista.botonBuscar) {
-            peli.setId(Integer.parseInt(vista.entradaID.getText()));
-
-            if (peliConsulta.buscar(peli)) {
-                vista.entradaID.setText(String.valueOf(peli.getId()));
-                vista.entradaNombre.setText(peli.getNombre());
-                vista.entradaGenero.setText(peli.getGenero());
-                vista.entradaAño.setText(String.valueOf(peli.getAño()));
-                vista.entradaDirector.setText(peli.getDirector());
-                vista.entradaPais.setText(peli.getPais());
-            } else {
-                JOptionPane.showMessageDialog(vista, "Error al buscar");
-                limpiar();
-            }
-        }
-        
-        if (e.getSource() == vista.botonLimpiar) {
+        if (peliConsulta.registrar(peli)) {
+            JOptionPane.showMessageDialog(vista, "Producto Agregado");
+            limpiar();
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error al agregar");
             limpiar();
         }
     }
 
-    public void limpiar() {
+    private void modificar(java.awt.event.ActionEvent evt) {
+        peli.setId(Integer.parseInt(vista.entradaID.getText()));
+        peli.setNombre(vista.entradaNombre.getText());
+        peli.setGenero(vista.entradaGenero.getText());
+        peli.setAño(Integer.parseInt(vista.entradaAño.getText()));
+        peli.setDirector(vista.entradaDirector.getText());
+        peli.setPais(vista.entradaPais.getText());
+
+        if (peliConsulta.modificar(peli)) {
+            JOptionPane.showMessageDialog(vista, "Producto modificado");
+            limpiar();
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error al modificar");
+            limpiar();
+        }
+    }
+
+    private void eliminar(java.awt.event.ActionEvent evt) {
+        peli.setId(Integer.parseInt(vista.entradaID.getText()));
+
+        if (peliConsulta.eliminar(peli)) {
+            JOptionPane.showMessageDialog(vista, "Producto eliminado");
+            limpiar();
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error al eliminar");
+            limpiar();
+        }
+
+    }
+
+    private void buscar(java.awt.event.ActionEvent evt) {
+        peli.setId(Integer.parseInt(vista.entradaID.getText()));
+
+        if (peliConsulta.buscar(peli)) {
+            vista.entradaID.setText(String.valueOf(peli.getId()));
+            vista.entradaNombre.setText(peli.getNombre());
+            vista.entradaGenero.setText(peli.getGenero());
+            vista.entradaAño.setText(String.valueOf(peli.getAño()));
+            vista.entradaDirector.setText(peli.getDirector());
+            vista.entradaPais.setText(peli.getPais());
+        } else {
+            JOptionPane.showMessageDialog(vista, "Error al buscar");
+            limpiar();
+        }
+    }
+
+    private void limpiar(java.awt.event.ActionEvent evt) {
+        limpiar();
+    }
+
+    private void limpiar() {
         vista.entradaID.setText(null);
         vista.entradaNombre.setText(null);
         vista.entradaGenero.setText(null);
